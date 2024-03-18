4a832e5ed28906b13501ccd8fc3f0a6ef2416c69
Igor Amantino <igor.amantinodev@hotmail.com>
2024-03-18 13:18:39 -0300

    feat/add-icons-in-tabs

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,8 +1,13 @@
-import { createBottomTabNavigator,BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator, BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+
 import { Exercise } from "@screens/Exercise";
 import { History } from "@screens/History";
 import { Home } from "@screens/Home";
 import { Profile } from "@screens/Profile";
+
+import HomeLogo from "@assets/home.svg"
+import HistoryLogo from "@assets/history.svg"
+import ProfileLogo from "@assets/profile.svg"
 
 type AppRoutes = {
   home: undefined;
@@ -17,10 +22,17 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
   return (
-    <Navigator screenOptions={{headerShown:false, tabBarShowLabel: false}}>
-      <Screen name="home" component={Home} />
-      <Screen name="history" component={History} />
-      <Screen name="profile" component={Profile} />
+    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      <Screen name="home" component={Home} options={{
+        tabBarIcon: () => <HomeLogo width={32} height={32} />,
+      }} />
+
+      <Screen name="history" component={History} options={{
+        tabBarIcon: () => <HistoryLogo width={32} height={32} />,
+      }} />
+      <Screen name="profile" component={Profile} options={{
+        tabBarIcon: () => <ProfileLogo width={32} height={32} />,
+      }} />
       <Screen name="exercise" component={Exercise} />
     </Navigator>
   )
