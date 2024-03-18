e92bd1ec4cabebcacf2bbb04ded1195864299965
Igor Amantino <igor.amantinodev@hotmail.com>
2024-03-18 13:40:55 -0300

    feat/refactor-tab-styles

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,3 +1,4 @@
+import { Platform } from "react-native";
 import { createBottomTabNavigator, BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import { useTheme } from "native-base";
 import { Exercise } from "@screens/Exercise";
@@ -21,12 +22,19 @@ export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
-  const {colors} = useTheme()
+  const {sizes,colors} = useTheme()
   return (
     <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false , 
         
         tabBarActiveTintColor: colors.green[500],
-        tabBarInactiveTintColor: colors.gray[200]
+        tabBarInactiveTintColor: colors.gray[200],
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? "auto" : 96,
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6]
+        }
         
         }}>
      
@@ -40,7 +48,9 @@ export function AppRoutes() {
       <Screen name="profile" component={Profile} options={{
         tabBarIcon: ({color}) => <ProfileLogo fill={color} width={32} height={32} />,
       }} />
-      <Screen name="exercise" component={Exercise} />
+      <Screen name="exercise" component={Exercise} options={{
+        tabBarButton: () => null,
+      }} />
     </Navigator>
   )
 }
